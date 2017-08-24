4ecbe892d71daf6dd10d4b90aaa6e2cebe39ff55
liminspace <igormeln@gmail.com>
2017-08-24 18:26:36 +0300

    Update transtool_compilemessages. Release v0.0.10

--- transtool/management/commands/transtool_compilemessages.py
+++ transtool/management/commands/transtool_compilemessages.py
@@ -1,6 +1,6 @@
 import os
 from django.core.management import BaseCommand, call_command
-from ...settings import TRANSTOOL_LOCALE_PATHS
+from ...settings import TRANSTOOL_PROJECT_BASE_DIR
 
 
 class Command(BaseCommand):
@@ -8,10 +8,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Compile all .po files...')
-        for locale_path, locale_opts in TRANSTOOL_LOCALE_PATHS:
-            locale_path = os.path.abspath(locale_path)
-            os.chdir(locale_path)
-            self.stdout.write('chdir {}'.format(locale_path))
-            self.stdout.write('run compilemessages')
-            call_command('compilemessages')
+        os.chdir(TRANSTOOL_PROJECT_BASE_DIR)
+        self.stdout.write('chdir {}'.format(TRANSTOOL_PROJECT_BASE_DIR))
+        self.stdout.write('run compilemessages')
+        # todo add supporting --use-fuzzy, --locale and --exclude args
+        call_command('compilemessages')
         self.stdout.write('Done.')
