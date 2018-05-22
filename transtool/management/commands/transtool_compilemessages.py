59abcca99e92e83ea7dda374d4a996af7fefda2d
liminspace <igormeln@gmail.com>
2018-05-22 16:56:05 +0300

    Update compilemessages.

--- transtool/management/commands/transtool_compilemessages.py
+++ transtool/management/commands/transtool_compilemessages.py
@@ -1,6 +1,7 @@
 import os
+import django
 from django.core.management import BaseCommand, call_command
-from ...settings import TRANSTOOL_PROJECT_BASE_DIR
+from ...settings import TRANSTOOL_PROJECT_BASE_DIR, TRANSTOOL_LOCALE_PATHS
 
 
 class Command(BaseCommand):
@@ -13,4 +14,12 @@ class Command(BaseCommand):
         self.stdout.write('run compilemessages')
         # todo add supporting --use-fuzzy, --locale and --exclude args
         call_command('compilemessages')
+        if django.VERSION < (1, 9):
+            for locale_paths_item in TRANSTOOL_LOCALE_PATHS:
+                locale_path = locale_paths_item[0]
+                os.chdir(locale_path)
+                self.stdout.write('chdir {}'.format(locale_path))
+                self.stdout.write('run compilemessages')
+                # todo add supporting --use-fuzzy, --locale and --exclude args
+                call_command('compilemessages')
         self.stdout.write('Done.')
